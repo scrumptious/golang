ac57d6078c3ad1492f4de963bdcf4dc6d4d1744a
ljasiurski <ljasiurski@gmail.com>
2022-03-13 16:32:28 +0000

    end of the tutorial3 - RESTful services

--- handlers/products.go
+++ handlers/products.go
@@ -15,9 +15,17 @@ func NewProducts(l *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(w, r)
+		return
+	}
+	//catch all other methods
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
 	list := data.GetProducts()
 	err := list.ToJSON(w)
-
 	if err != nil {
 		http.Error(w, "Unable to convert response to JSON", http.StatusInternalServerError)
 	}
